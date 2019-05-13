a1b879e04d3cf34f7d10102285495ef7e93433c0
fzingithub <1194585271@qq.com>
2019-05-13 15:12:43 +0800

    5.13

--- 6_SGD/2_0_sigmoid_grad.py
+++ 6_SGD/2_0_sigmoid_grad.py
@@ -1,5 +1,7 @@
 import tensorflow as tf 
+import os
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 a = tf.linspace(-10., 10., 10)
 
---
+++ 6_SGD/2_2_relu.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2019/5/9
+Author: zhe
+Email: 1194585271@qq.com
+'''
+import tensorflow as tf
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+a = tf.linspace(-10., 10., 10)
+
+print(tf.nn.relu(a))
+
+print(tf.nn.leaky_relu(a))
+
+
